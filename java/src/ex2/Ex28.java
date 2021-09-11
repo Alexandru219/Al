7cca38d853d0408fb1141a93ae3dc8e5739e3eb7
Alex <alexandrudjundiet@gmail.com>
2021-09-11 09:02:35 +0300

    lab1

---
+++ java/src/ex2/Ex28.java
@@ -0,0 +1,21 @@
+package ex2;
+
+import java.util.Scanner;
+
+public class Ex28 {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        int radius = 0;
+        double pi = Math.PI;
+
+        System.out.println("Enter radius:");
+        radius = input.nextInt();
+
+        System.out.printf("Diameter =  %.1f\n", 2 * (double) radius);
+
+        System.out.printf("Circumference =  %.1f\n", 2 * pi * (double) radius);
+
+        System.out.printf("Area =  %.1f", pi * (double) (radius * radius));
+    }
+}
